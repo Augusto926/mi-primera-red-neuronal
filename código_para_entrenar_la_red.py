05ab6ec0d2893d45faf3444148e78cbdb4d660dc
Augusto <131948472+Augusto926@users.noreply.github.com>
2025-08-28 08:45:19 -0600

    Agregué dos lineas de código para convertir training_data y test_data en listas.

--- código_para_entrenar_la_red.py
+++ código_para_entrenar_la_red.py
@@ -2,4 +2,12 @@ import mnist_loader
 import network
 import pickle
 
+# Definir las salidas
 training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
+
+# Convertir en listas
+training_data = list(training_data)
+test_data = list(test_data)
+
+
+
