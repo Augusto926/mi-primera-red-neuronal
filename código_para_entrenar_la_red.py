a200c51593c4eda47c237a7ba00d6668f1a46013
Augusto <131948472+Augusto926@users.noreply.github.com>
2025-08-28 09:03:29 -0600

    Definí los hiperparamétros del metodo "GSD" de la clase "network"

--- código_para_entrenar_la_red.py
+++ código_para_entrenar_la_red.py
@@ -2,13 +2,14 @@ import mnist_loader
 import network
 import pickle
 
-# Definir las salidas
+# Define la salida de mnist_loader.load_data_wrapper()
 training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 
 # Convertir en listas
 training_data = list(training_data)
 test_data = list(test_data)
 
+# Crea la arquitectura de la red
 net = network.Network([784,30,10])
 """
 Crea una instancia de la clase Network con la arquitectura:
@@ -18,5 +19,17 @@ Crea una instancia de la clase Network con la arquitectura:
 30 neuronas en la capa oculta,
 
 10 neuronas en la capa de salida.
+"""
+
+# Define los hiperparamétros del metodo "GSD" de la clase "network"
+net.SGD(training_data, 30, 10, 0.1, test_data=test_data)
+"""
+El valor de cada hiperparametro es:
+
+epochs=30,
+
+mini_batch_size=10,
+
+eta=0.1.
 """
 
