c93a22fa7da7f91bbd44995dd35008ca1854ebe0
Augusto <131948472+Augusto926@users.noreply.github.com>
2025-08-28 08:49:55 -0600

    Creé y definí la arquitectura de mi red neuronal básica.

--- código_para_entrenar_la_red.py
+++ código_para_entrenar_la_red.py
@@ -9,5 +9,14 @@ training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
 
+net = network.Network([784,30,10])
+"""
+Crea una instancia de la clase Network con la arquitectura:
 
+784 neuronas en la capa de entrada,
+
+30 neuronas en la capa oculta,
+
+10 neuronas en la capa de salida.
+"""
 
