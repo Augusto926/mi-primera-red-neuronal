fbc0f0c5d7a3e3a6fad27817293eef15bce8c34f
Augusto <131948472+Augusto926@users.noreply.github.com>
2025-09-02 08:49:21 -0600

    Define el método (función) de la función de costo cross entropy.

--- cross_entropy_cost.py
+++ cross_entropy_cost.py
@@ -1,5 +1,14 @@
 # Esta clase agrupa la lógica de la función de costo de Entropía Cruzada (cross-entropy).
 class CrossEntropyCost(object): 
+
     @staticmethod 
+    def fn(a, y):
+        """
+        Retorna el costo asociado a la salida "a" y la salida deseada "y". Nota que np.nan_to_num 
+        se usa para tener estabilidad númerrica. Si "a" o "y" tienen 1.0 en las mismas entradas, 
+        la expresión (1-y)*np.log(1-a) puede producir nan por log(0), y np.nan_to_num corrige esos nan a 0.0. 
+        np.sum(...) suma todos los elementos resultantes y devuelve el costo escalar total. 
+        """    
+        return np.sum(-y*np.log(a)-(1-y)*np.log(1-a))
 
 
