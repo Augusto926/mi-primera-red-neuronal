516f1e257c9d9cc47ffde423c7e554aaa5148bdd
Augusto <131948472+Augusto926@users.noreply.github.com>
2025-09-02 09:23:33 -0600

    Genere una rama llamada primera-mejora-red-network.py. Agregue el contenido de mi archivo cross_entropy_cost.py a mi archivo network.py.

--- network.py
+++ network.py
@@ -16,6 +16,25 @@ import random
 # Third-party libraries
 import numpy as np
 
+class CrossEntropyCost(object): 
+
+    @staticmethod 
+    def fn(a, y):
+        """
+        Retorna el costo asociado a la salida "a" y la salida deseada "y". Nota que np.nan_to_num 
+        se usa para tener estabilidad númerrica. Si "a" o "y" tienen 1.0 en las mismas entradas, 
+        la expresión (1-y)*np.log(1-a) puede producir nan por log(0), y np.nan_to_num corrige esos nan a 0.0. 
+        np.sum(...) suma todos los elementos resultantes y devuelve el costo escalar total. 
+        """    
+        return np.sum(-y*np.log(a)-(1-y)*np.log(1-a))
+    
+    def delta(a, y):
+        """
+        Retorna la señal de error en la capa de salida 
+        (el vector δ = ∂C/∂z para la capa de salida).
+        """
+        return (a-y)
+    
 class Network(object):
 
     def __init__(self, sizes):
